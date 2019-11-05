023bc98f2b46ebad69c67174af070d254319c631
orz0219 <lazymax0219@gmail.com>
2019-11-05 11:42:42 +0800

    change

--- lazyvue/src/main.js
+++ lazyvue/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App.vue'
+import 'at-ui-style'
+import AtUI from 'at-ui'
+import routes from '@/lib/router'
+import axiosUtil from "@/lib/axios";
 
 Vue.config.productionTip = false
+Vue.use(Router)
+Vue.use(AtUI)
+Vue.use(axiosUtil)
+
+const router = new Router({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
